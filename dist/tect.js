2cecf6b4eda1b809f3828e0337727a92dba0afc3
Salah Eddine Soussou <88331708+SalahSoussou@users.noreply.github.com>
2022-07-29 20:36:48 +0100

    ok

--- dist/tect.js
+++ dist/tect.js
@@ -6,4 +6,6 @@ let hire = true;
 names = '2';
 let arr = [1, 2, 3, 4];
 let arr2 = ['1', '2', '3', '4'];
+let arr3 = [1, 2, 3, 4, 'A', 'B', 'C'];
+let arr4 = [1, 2, 3, 4, 'A', 'B', 'C', ['D', 'e', 2]];
 //# sourceMappingURL=tect.js.map
