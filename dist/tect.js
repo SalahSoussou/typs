5e3130a00e00a1499c0bcf2f8747c29f871456f6
Salah Eddine Soussou <88331708+SalahSoussou@users.noreply.github.com>
2022-07-29 20:19:52 +0100

    my typs

---
+++ dist/tect.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let names = 'salah';
+let theAge = 23;
+let hire = true;
+names = '2';
+let arr = [1, 2, 3, 4];
+let arr2 = ['1', '2', '3', '4'];
+//# sourceMappingURL=tect.js.map
---
+++ src/tect.ts
@@ -0,0 +1,10 @@
+export{}
+
+let names: string = 'salah';
+let theAge: number = 23;
+let  hire: boolean = true;
+
+names = '2'
+
+let arr : number[] = [1,2,3,4]
+let arr2 : string[] = ['1','2','3','4']
