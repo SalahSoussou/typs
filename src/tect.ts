fb9895c2b54c9e63c38f0b9e507f43501150f9bc
Salah Eddine Soussou <88331708+SalahSoussou@users.noreply.github.com>
2022-07-29 20:36:10 +0100

    ok

--- src/tect.ts
+++ src/tect.ts
@@ -8,3 +8,7 @@ names = '2'
 
 let arr : number[] = [1,2,3,4]
 let arr2 : string[] = ['1','2','3','4']
+let arr3 : (string|number)[] = [1,2,3,4,'A','B','C']
+
+
+let arr4 : (string|number|(string|number)[])[] = [1,2,3,4,'A','B','C',['D','e',2]]
