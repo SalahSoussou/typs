d487e89c08fbf016fcb0190f269c1414df8b8cc6
SalahSoussou <kaka79702@gmail.com>
2022-09-04 12:16:48 +0100

    ok

--- dist/test.js
+++ dist/test.js
@@ -1,2 +1,24 @@
 "use strict";
+console.log('hi');
+function compare(num1, num2) {
+    return num1 === num2 ? 0 : num1 < num2 ? 1 : -1;
+}
+let data2 = [22, 'Title', true];
+var Kids;
+(function (Kids) {
+    Kids[Kids["five"] = 7] = "five";
+    Kids[Kids["six"] = 8] = "six";
+    Kids[Kids["seven"] = 9] = "seven";
+})(Kids || (Kids = {}));
+var Value;
+(function (Value) {
+    Value[Value["small"] = 9] = "small";
+    Value[Value["mid"] = 20] = "mid";
+    Value[Value["large"] = 30] = "large";
+    Value[Value["xlarge"] = 40] = "xlarge";
+})(Value || (Value = {}));
+let size = "s";
+if (size === 's')
+    console.log(`your size is ${Value.small}`);
+let myImg = document.getElementById('bgImg');
 //# sourceMappingURL=test.js.map
