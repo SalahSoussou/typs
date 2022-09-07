f29db970575c38bd855c99c55ec4bc6d79c9a43d
SalahSoussou <kaka79702@gmail.com>
2022-09-07 11:09:44 +0100

    end of the cours

--- dist/deep.js
+++ dist/deep.js
@@ -14,4 +14,74 @@ class cUser {
 }
 let userOne = new cUser('salae', 345);
 console.log(userOne.meth());
+class Static {
+    constructor(uname) {
+        this.uname = uname;
+        Static.createdObjs++;
+    }
+    static getCount() {
+        console.log(`${this.createdObjs} objects created`);
+    }
+}
+Static.createdObjs = 0;
+let u1 = new Static('ala');
+let u2 = new Static('hello');
+let u3 = new Static('test');
+let u4 = new Static('hi');
+Static.getCount();
+class Player {
+    constructor(name) {
+        this.name = name;
+    }
+    attack() {
+        console.log('attack');
+    }
+}
+class Colt extends Player {
+    constructor(name, bulits) {
+        super(name);
+        this.bulits = bulits;
+    }
+    attack() {
+        super.attack();
+        console.log('boulits');
+        this.bulits -= 1;
+    }
+}
+class King extends Player {
+    constructor(name, axeHp) {
+        super(name);
+        this.axeHp = axeHp;
+    }
+    attack() {
+        super.attack();
+        console.log('axe');
+        this.axeHp -= 1;
+    }
+}
+let king = new King('barb', 100);
+console.log(king.name);
+king.attack();
+console.log(king.axeHp);
+function retuenAnyType(val) {
+    return val;
+}
+console.log(retuenAnyType(100));
+console.log(retuenAnyType('Salah'));
+console.log(retuenAnyType(true));
+function retuenString(val, num) {
+    return `${val}and ${num} in string format`;
+}
+class Collection {
+    constructor() {
+        this.data = [];
+    }
+    add(item) {
+        this.data.push(item);
+    }
+}
+let itemOne = new Collection();
+itemOne.add({ itemtype: 'Book', title: 'my Book', id: 235 });
+itemOne.add({ itemtype: 'BookY :D', title: 'my Book 2', id: 888885 });
+console.log(itemOne);
 //# sourceMappingURL=deep.js.map
--- src/deep.ts
+++ src/deep.ts
@@ -20,3 +20,92 @@ let userOne = new cUser('salae', 345)
 
 console.log(userOne.meth())
 
+class Static {
+    static createdObjs: num = 0
+    static getCount(): void {
+        console.log(`${this.createdObjs} objects created`)
+    }
+    constructor(public uname: str) {
+        Static.createdObjs++
+    }
+}
+
+let u1 = new Static('ala')
+let u2 = new Static('hello')
+let u3 = new Static('test')
+let u4 = new Static('hi')
+
+Static.getCount()
+
+// Polymorphism ================
+
+
+class Player {
+    constructor(public name: str) { }
+    attack(): void {
+        console.log('attack')
+    }
+}
+
+class Colt extends Player {
+    constructor(name: str, public bulits: num) {
+        super(name)
+    }
+    override attack(): void {
+        super.attack()
+        console.log('boulits')
+        this.bulits -= 1
+    }
+}
+
+class King extends Player {
+    constructor(name: str, public axeHp: num) {
+        super(name)
+    }
+    override attack(): void {
+        super.attack()
+        console.log('axe')
+        this.axeHp -= 1
+    }
+}
+
+let king = new King('barb', 100)
+
+console.log(king.name)
+king.attack()
+console.log(king.axeHp)
+
+// ====================================================================
+
+function retuenAnyType<T>(val: T): T {
+    return val;
+}
+
+console.log(retuenAnyType<num>(100))
+console.log(retuenAnyType<str>('Salah'))
+console.log(retuenAnyType<bl>(true))
+
+function retuenString<S, N>(val: S, num: N): string {
+    return `${val}and ${num} in string format`;
+}
+
+
+//++++++++++++++++++++++++++++++++++++
+interface Book {
+    itemtype: string;
+    title: string;
+    id: number
+}
+
+class Collection<T>{
+    public data: T[] = []
+    add(item: T): void {
+        this.data.push(item)
+    }
+}
+
+let itemOne = new Collection<Book>()
+itemOne.add({ itemtype: 'Book', title: 'my Book', id: 235 })
+itemOne.add({ itemtype: 'BookY :D', title: 'my Book 2', id: 888885 })
+
+console.log(itemOne)
